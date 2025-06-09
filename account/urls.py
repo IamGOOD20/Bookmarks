f2fefeff03a047a5b2f6d6bf4bf94a63ae639526
IamGOOD <e.rukshenas@gmail.com>
2025-06-09 12:19:37 -0400

    Log in and log out functionality implemented; debbuged static issues

--- account/urls.py
+++ account/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('login/', views.user_login, name='login'),
-    # login / logout url
-    #path('login/', auth_views.LoginView.as_view(), name='login'),
-    #path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+     #path('login/', views.user_login, name='login'),
+     #login / logout url
+     path('login/', auth_views.LoginView.as_view(), name='login'),
+     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+     path('', views.dashboard, name='dashboard'),
 ]
 
